6003ff33d4f878effa644ca4cdaa84df2f0f252c
Cyrus Harmon <ch-github@bobobeach.com>
2012-03-29 22:46:58 -0700

    fix dependencies, clean up project.clj

--- project.clj
+++ project.clj
@@ -1,16 +1,7 @@
 
-
-(defproject cdk-hacking "0.0.0"
-  :description "A toy project for playing around with CDK"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.freehep/freehep-graphics2d "2.1.1"]
+(defproject abcl-cdk-hacking "0.0.0"
+  :description "Fake project for fetching abcl-cdk-hacking dependencies"
+  :dependencies [[org.freehep/freehep-graphics2d "2.1.1"]
                  [org.freehep/freehep-graphicsio-pdf "2.1.1"]
-                 [org.freehep/freehep-graphicsio-svg "2.1.1"]
-                 [org.openscience/cdk "1.4.7"]
-                 [junit "3.8.2"]
-                 [org.apache.maven/maven-aether-provider "3.0.4"]
-                 [org.apache.maven.wagon/wagon-http "2.2"]
- ]
-
-  :dev-dependencies [[swank-clojure "1.5.0-SNAPSHOT"]]
+                 [org.freehep/freehep-graphicsio-svg "2.1.1"]]
   :repositories {"freehep" "http://java.freehep.org/maven2"})
