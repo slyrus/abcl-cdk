926f2d4dcc8c2f3e8d87322e6423ff2e5b1e12f7
Cyrus Harmon <ch-github@bobobeach.com>
2013-12-31 13:43:09 -1000

    update probably unused project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 
 (defproject abcl-cdk "0.0.0"
   :description "Fake project for fetching abcl-cdk dependencies"
-  :dependencies [[org.freehep/freehep-graphics2d "2.1.1"]
-                 [org.freehep/freehep-graphicsio-pdf "2.1.1"]
-                 [org.freehep/freehep-graphicsio-svg "2.1.1"]]
+  :dependencies [[org.freehep/freehep-graphics2d "2.2.1"]
+                 [org.freehep/freehep-graphicsio-pdf "2.2.1"]
+                 [org.freehep/freehep-graphicsio-svg "2.2.1"]]
   :repositories {"freehep" "http://java.freehep.org/maven2"})
