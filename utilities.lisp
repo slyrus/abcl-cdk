9e551d1f53d1c55bc4ee6a25b640045edcd4459c
Cyrus Harmon <ch-github@bobobeach.com>
2014-08-21 17:22:39 -0700

    add reorder-atoms and friends

--- utilities.lisp
+++ utilities.lisp
@@ -38,13 +38,22 @@
                    (symbol-name (quote ,class))))))
 
 (jimport |java.util| |Vector|)
+(jimport |java.util| |Arrays|)
 (jimport |java.lang| |Integer|)
 (jimport |org.openscience.cdk| |CDKConstants|)
+(jimport |org.openscience.cdk.interfaces| |IAtom|)
+(jimport |org.openscience.cdk.interfaces| |IBond|)
 (jimport |org.openscience.cdk.config| |Isotopes|)
 (jimport |org.openscience.cdk.interfaces| |IPseudoAtom|)
 (jimport |org.openscience.cdk| |AtomContainer|)
 (jimport |org.openscience.cdk.graph| |ShortestPaths|)
 (jimport |org.openscience.cdk.ringsearch| |AllRingsFinder|)
+
+(jimport |org.openscience.cdk.tools.manipulator| |MolecularFormulaManipulator|)
+(jimport |org.openscience.cdk.tools.manipulator| |AtomContainerManipulator|)
+
+(jimport |org.openscience.cdk.graph.invariant| |Canon|)
+(jimport |org.openscience.cdk.graph| |GraphUtil|)
 
 (defun jlist (&rest initial-contents)
   (sequence:make-sequence-like
@@ -178,3 +187,81 @@ atoms-to-keep (and their associated bonds) from ac."
   "Create a (shallow) copy of an atom container."
   (java:jnew |AtomContainer| ac))
 
+
+(defun reorder-atoms (ac)
+  (let ((labels (#"label" '#.(make-symbol |Canon|) ac
+                          (#0"toAdjList" '#.(make-symbol |GraphUtil|) ac))))
+    (let ((atoms (#"getAtomArray" '#.(make-symbol |AtomContainerManipulator|) ac))
+          (bonds (#"getBondArray" '#.(make-symbol |AtomContainerManipulator|) ac)))
+      ;; set atom ranks
+      (loop for atom across atoms
+         for i from 0
+         do (#"setProperty" atom "rank" (java:jarray-ref labels i)))
+
+      (sort atoms (lambda (a b)
+                    (let ((a-rank (#"getProperty" a "rank" (java:jclass "java.lang.Integer")))
+                          (b-rank (#"getProperty" b "rank" (java:jclass "java.lang.Integer"))))
+                      (> a-rank b-rank))))
+
+      (sort bonds (lambda (a b)
+                    (let ((a0 (#"getProperty" (#"getAtom" a 0) "rank" (java:jclass "java.lang.Integer")))
+                          (a1 (#"getProperty" (#"getAtom" a 1) "rank" (java:jclass "java.lang.Integer")))
+                          (b0 (#"getProperty" (#"getAtom" b 0) "rank" (java:jclass "java.lang.Integer")))
+                          (b1 (#"getProperty" (#"getAtom" b 1) "rank" (java:jclass "java.lang.Integer"))))
+                      (if (= (min a0 a1) (min b0 b1))
+                          (> (max a0 a1) (max b0 b1))
+                          (> (min a0 a1) (min b0 b1))))))
+      
+      (#"setAtoms" ac (java:jnew-array-from-array |IAtom| atoms))
+      (#"setBonds" ac (java:jnew-array-from-array |IBond| bonds))
+      
+      ;; clean up
+      (loop for atom across atoms
+         for i from 0
+         do
+           (#"removeProperty" atom "rank")
+           (when (null (#"getProperties" atom))
+             (#"setProperties" atom java:+null+)))))
+  ac)
+
+#|
+
+final long[] labels = Canon.label(m, GraphUtil.toAdjList(m));
+ 
+IAtom[] atoms = AtomContainerManipulator.getAtomArray(m);
+IBond[] bonds = AtomContainerManipulator.getBondArray(m);
+ 
+// atoms don't know their index
+for (int i = 0; i < labels.length; i++)
+  atoms[i].setProperty("rank", labels);
+  
+// sort atoms and bonds (ensure neighbours are provided in the same order)
+Arrays.sort(atoms, new Comparator<IAtom>() {
+    @Override public int compare(IAtom a, IAtom b) {
+        return a.getProperty("rank", Long.class)
+                .compareTo(a.getProperty("rank", Long.class));
+    }
+});
+Arrays.sort(bonds, new Comparator<IBond>() {
+    @Override public int compare(IBond a, IBond b) {
+        long a1 = a.getAtom(0).getProperty("rank");
+        long a2 = a.getAtom(1).getProperty("rank");
+        long b1 = b.getAtom(0).getProperty("rank");
+        long b2 = b.getAtom(1).getProperty("rank");
+        int cmp = Longs.compare(Math.min(a1, a2), Math.min(b1, b2));
+        return cmp != 0 ? cmp : Longs.compare(Math.max(a1, a2), Math.max(b1, b2)); 
+    }
+});
+ 
+// set the new orderings
+m.setAtoms(atoms);
+m.setBonds(bonds);
+ 
+// clean up
+for (IAtom a : m.atoms()) {
+    a.removeProperty("rank");
+    if (a.getProperties().isEmpty())
+        a.setProperties(null);
+}
+
+|#
